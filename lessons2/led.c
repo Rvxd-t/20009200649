2a4cc25717dc83654a02c4f3f9e54cad811ce00f
Rvxd-t <87228990+Rvxd-t@users.noreply.github.com>
2021-07-16 19:09:48 +0800

    Update led.c

--- lessons2/led.c
+++ lessons2/led.c
@@ -1,1 +1,26 @@
+// C++ code
+//
+int i=0;
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+}
 
+void loop()
+{
+  for(i=13;i>4;i--)
+  {
+  	digitalWrite(i, HIGH);
+ 	delay(1000); // Wait for 1000 millisecond(s)
+  	digitalWrite(i, LOW);
+  	delay(1000); // Wait for 1000 millisecond(s)
+  }
+}
